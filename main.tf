28b7880696a183e40652f183b3222af230b0befa
Ben Wart <benwart@gmail.com>
2022-10-14 18:30:00 -0700

    wip

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "github_repository" "example" {
+  name        = "example"
+  description = "My awesome codebase"
+  visibility  = "public"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "name-prefix" {
+  description = "Prefix for repository name."
+  type        = string
+  default     = ""
+}
+
+variable "name-suffix" {
+  description = "Suffix for repository name."
+  type        = string
+  default     = ""
+}
